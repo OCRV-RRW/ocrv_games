11f3d14fa1696636f8ac8af875abc708b81111b6
MomusWinner <areg06.06@mail.ru>
2024-12-09 12:59:20 +0300

    fix cors

--- main.go
+++ main.go
@@ -39,8 +39,8 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language," +
 			"Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
-		AllowOrigins:     "http://localhost:3000",
-		AllowCredentials: true,
+		AllowOrigins:     "*",
+		AllowCredentials: false,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
