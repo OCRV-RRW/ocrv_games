b54eedd20e8193fc096d1ce4dff68468cf6a6e56
MomusWinner <areg06.06@mail.ru>
2025-01-16 13:13:05 +0300

    add med.game-ocrv.ru to cors

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language," +
 			"Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
-		AllowOrigins:     "http://localhost:3000,http://localhost:8000",
+		AllowOrigins:     "http://localhost:3000,http://localhost:8000,https://med.ocrv-game.ru",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
